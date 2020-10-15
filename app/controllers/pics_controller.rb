ec8e7a65fb8100504a48230f7a8e91cfc915f507
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-10-15 12:37:35 -0500

    added show and index action, added some content to index and show views

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,6 +1,11 @@
 class PicsController < ApplicationController
+  before_action :set_pic ,only: %i[show edit update destroy]
+
   def index
-    
+    @pics = Pic.all
+  end
+
+  def show
   end
 
   def new
@@ -9,6 +14,12 @@ class PicsController < ApplicationController
 
   def create
     @pic = Pic.create(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: 'Pic posted!'
+    else
+      render 'new'
+    end
   end
 
   private
@@ -16,5 +27,9 @@ class PicsController < ApplicationController
   def pic_params
     params.require(:pic).permit(:title, :description)
   end
-  
+
+  def set_pic
+    @pic = Pic.find(params[:id])
+  end
+
 end
